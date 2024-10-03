46e2eb53f9a56fe7e5e005f50174523036c525e0
Sagnik8103 <majumdarsagnik3@gmail.com>
2024-10-03 17:52:56 +0000

    configure done

---
+++ jscodes/s2.js
@@ -0,0 +1,12 @@
+const accountId=14453
+let accountEmail="majumdarsagnik3@gmail.com"
+var accountPassword="12345"
+acountCity="Jaipur"
+let accountState;
+
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+acountCity="Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,acountCity,accountState])
