8cf9e850a3a2cf2f277696fcab2421edee2cb411
Sagnik8103 <majumdarsagnik3@gmail.com>
2024-10-04 09:00:01 +0000

    config done

---
+++ jscodes/s3.js
@@ -0,0 +1,15 @@
+let value=3
+let negValue=-value
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**2)
+
+let str1="SAGNIK "
+let str2="MAJUMDAR"
+let str3=str1 + str2
+console.log(str3);
+
